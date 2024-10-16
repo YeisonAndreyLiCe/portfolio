bf466944ff275629eca228e9ee38e66f3301919a
Yeison Liscano <yliscanoc@unal.edu.co>
2024-10-15 20:02:36 -0500

    config\test(front): #4 add cypress util
    - add util to run cypress using nix

---
+++ cypress/main.nix
@@ -0,0 +1,49 @@
+{ inputs, isLinux, makeTemplate, __system__, ... }:
+let
+  version = "13.13.3";
+  srcs = {
+    aarch64-linux = {
+      platform = "linux-arm64";
+      sha256 = "sha256-VgX8xaYCwyjAZh1HZOooFygUUyPnk2nAO9WvfieQyA0=";
+    };
+    x86_64-linux = {
+      platform = "linux-x64";
+      sha256 = "sha256-3Imr4KA4n47Ja9W4Wwl9UfSler3O2W5eJ6nxrHItuxE=";
+    };
+  };
+  currentPlatform = builtins.currentSystem;
+  platformInfo = builtins.getAttr currentPlatform srcs;
+  bin = if isLinux then
+    (inputs.nixpkgs.cypress.overrideAttrs (_: {
+      inherit version;
+      src = inputs.nixpkgs.fetchzip {
+        url =
+          "https://cdn.cypress.io/desktop/${version}/${platformInfo.platform}/cypress.zip";
+        sha256 = "${platformInfo.sha256}";
+      };
+    }))
+  else
+    inputs.nixpkgs.fetchzip {
+      url = "https://cdn.cypress.io/desktop/13.13.3/darwin-arm64/cypress.zip";
+      sha256 = "sha256-a0Pe+GsH07Ru55PFndp/oHVLPbs/VamYJd2pEJwbVjQ=";
+      stripRoot = false;
+    };
+in makeTemplate {
+  template = ./template.sh;
+  name = "cypress-fa";
+  replace = {
+    __argCypress__ = bin;
+    __argIsLinux__ = builtins.toString isLinux;
+  };
+  searchPaths = {
+    bin = [ inputs.nixpkgs.bash inputs.nixpkgs.git inputs.nixpkgs.nodejs_22 ]
+      ++ inputs.nixpkgs.lib.optionals isLinux [
+        inputs.nixpkgs.cypress
+        inputs.nixpkgs.gnugrep
+        inputs.nixpkgs.gnused
+        inputs.nixpkgs.firefox
+        inputs.nixpkgs.chromium
+        inputs.nixpkgs.xvfb-run # required for headless mode
+      ];
+  };
+}
---
+++ cypress/template.sh
@@ -0,0 +1,45 @@
+# shellcheck shell=bash
+
+function _create_tmp_dir {
+  local source="${1}"
+  local target="${2}"
+
+  : && info "Cleaning ${target}" \
+    && rm -rf "${target}" \
+    && info "Copying ${source} to ${target}" \
+    && copy "${source}" "${target}"
+}
+
+function run_cypress_fa {
+  local browser="${1-}"
+  local action="${2-}"
+  local spec="${3-}"
+  local cypress_bin
+  local cypress_run
+  local custom_tmp_dir="custom/tmp"
+  export CYPRESS_RUN_BINARY
+
+  : \
+    && mkdir -p "${custom_tmp_dir}" \
+    && export TMPDIR="${custom_tmp_dir}" \
+    && cypress_bin="${PWD}/cypress" \
+    && if test -n "${spec}"; then
+      : && info "Running cypress tests only for ${spec}" \
+        && args+=(--spec "${spec}")
+    else
+      info "Running cypress tests for all specs"
+    fi \
+    && _create_tmp_dir "__argCypress__/." "${cypress_bin}" \
+    && if test -n "__argIsLinux__"; then
+      CYPRESS_RUN_BINARY="${cypress_bin}/bin/Cypress"
+      cypress_run=(xvfb-run npx cypress run --browser "${browser}" --headless "${args[@]}")
+    else
+      CYPRESS_RUN_BINARY="${cypress_bin}/Cypress.app/Contents/MacOS/Cypress"
+      cypress_run=(npx cypress run --browser electron "${args[@]}")
+    fi \
+    && case "${action}" in
+      run) "${cypress_run[@]}" ;;
+      open) npx cypress open ;;
+      *) error 'First argument must be one of: run, open' ;;
+    esac
+}
