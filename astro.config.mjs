76d278231f83bf8e1f5510106d59998bbec8caa1
Yeison Liscano <yliscanoc@unal.edu.co>
2024-10-20 17:55:31 -0500

    style\feat(front): #2 update mobile styles
    - imporve respossiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "tokyo-night",
+      wrap: true,
     },
   },
   integrations: [
@@ -19,6 +20,7 @@ export default defineConfig({
       syntaxHighlight: "shiki",
       shikiConfig: {
         theme: "tokyo-night",
+        wrap: true,
       },
     }),
     sitemap(),
