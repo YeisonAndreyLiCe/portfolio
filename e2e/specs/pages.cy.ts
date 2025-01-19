88476c8086bf5ec278dfd41743f39dac98aff4a0
Yeison Liscano <yliscanoc@unal.edu.co>
2025-01-19 12:01:17 -0500

    style\refac(front): #2 home view
    - update home view
    - chage dark sidebar color
    - add higlight feature to toc

--- e2e/specs/pages.cy.ts
+++ e2e/specs/pages.cy.ts
@@ -1,5 +1,6 @@
 describe("Web site", (): void => {
   it("Navigation", (): void => {
+    localStorage.setItem("theme", "light");
     cy.visit("/");
     cy.contains("Yeison Liscano").click();
 
@@ -9,5 +10,13 @@ describe("Web site", (): void => {
 
     cy.contains("posts").click();
     cy.contains("Read Post");
+    cy.get("#main-content").children().first().contains("Read Post").click();
+    cy.contains("Table of contents");
+
+    cy.get("html").should("have.attr", "data-theme", "light");
+    cy.get("#theme-toggle").click();
+    cy.get("html").should("have.attr", "data-theme", "dark");
+    cy.get("#theme-toggle").click();
+    cy.get("html").should("have.attr", "data-theme", "light");
   });
 });
